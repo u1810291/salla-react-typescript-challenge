ae55c3ddc4813abdcebc3bb73c2f1df1e0686cf1
u1810291 <otabekbutcher@gmail.com>
2024-07-12 19:13:54 +0500

    fix: fixed images

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   distDir: './dist', // Changes the build output directory to `./dist/`.
-  images: {
-    loader: 'custom',
-    loaderFile: '/public/images/icons/spinner.svg',
-  },
   env: {
     API_URL: process.env.API_URL
   }
