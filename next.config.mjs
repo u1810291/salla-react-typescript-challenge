e9193174689cc309e7d0a1a082714558af903e4f
u1810291 <otabekbutcher@gmail.com>
2024-07-12 19:08:24 +0500

    fix: fixed images

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,8 @@ const nextConfig = {
   reactStrictMode: false,
   distDir: './dist', // Changes the build output directory to `./dist/`.
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.salla.network',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
+    loader: 'custom',
+    loaderFile: './my/image/loader.js',
   },
   env: {
     API_URL: process.env.API_URL
